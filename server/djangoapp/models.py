bd5991cda6c778a4b6f0508e3c6040108e058a5d
ThoLe <129922615+thole1986@users.noreply.github.com>
2024-04-07 15:27:09 +0700

    Update models.py

--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -13,18 +13,16 @@ class CarModel(models.Model):
     make = models.ForeignKey(CarMake, null=True, on_delete=models.CASCADE)
     name = models.CharField(max_length=255)
     dealer_id = models.IntegerField()
-    
     SEDAN="Sedan"
     SUV="SUV"
     WAGON="WAGON"
-    
     TYPE_CHOICES = (
-        (SEDAN, "Sedan"),
-        (SUV, "SUV"),
-        (WAGON, "Wagon"),
+        (SEDAN,"Sedan"),
+        (SUV,"SUV"),
+        (WAGON,"Wagon"),
     )
     model_type = models.CharField(max_length=10, choices=TYPE_CHOICES)
     year = models.DateField()
-
+    
     def __str__(self):
         return f"{self.make} {self.name} ({self.year})"
